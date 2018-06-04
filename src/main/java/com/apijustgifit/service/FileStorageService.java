a92b0b6c14004a5f466360db7e697b09e8e9ba42
Lais Silva Gomes <lsgomes@thoughtworks.com>
2018-06-04 10:46:13 -0300

    [Lais] [N/A] Add loadFile method in FileStorageService class

--- src/main/java/com/apijustgifit/service/FileStorageService.java
+++ src/main/java/com/apijustgifit/service/FileStorageService.java
@@ -5,12 +5,15 @@ import com.apijustgifit.domain.StorageProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -27,7 +30,7 @@ public class FileStorageService {
         .toAbsolutePath().normalize();
     }
 
-    public String store(MultipartFile file) {
+    public String storeFile(MultipartFile file) throws IOException {
 
         String filename = StringUtils.cleanPath(file.getOriginalFilename());
 
@@ -38,10 +41,23 @@ public class FileStorageService {
             return filename;
 
         }catch (IOException e) {
-            e.printStackTrace();
-            log.info("------>"+e.getMessage());
-            return "";
+            throw e;
         }
 
+    }
+
+    public Resource loadFile(String fileName) throws MalformedURLException {
+
+        Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+
+        try {
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()){
+                return resource;
+            }
+        } catch (MalformedURLException e) {
+            throw e;
+        }
+        return null;
     }
 }
--- src/test/java/com/apijustgifit/FileStorageServiceTests.java
+++ src/test/java/com/apijustgifit/FileStorageServiceTests.java
@@ -2,13 +2,18 @@ package com.apijustgifit;
 
 import com.apijustgifit.domain.StorageProperties;
 import com.apijustgifit.service.FileStorageService;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.mock.web.MockMultipartFile;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -21,15 +26,18 @@ public class FileStorageServiceTests {
 
 	private StorageProperties fileStorageProperties;
 
+	private MockMultipartFile file;
 	@Mock
     FileInputStream inputStream;
 
+	String testFileName;
 	@Before
 	public void setUp() throws Exception {
 		fileStorageProperties = mock(StorageProperties.class);
 		when(fileStorageProperties.getUploadDir())
-				.thenReturn("");
+				.thenReturn("uploads/");
 		fileStorageService = new FileStorageService(fileStorageProperties);
+
 	}
 
 	@Test
@@ -38,14 +46,29 @@ public class FileStorageServiceTests {
 		MockMultipartFile file = new MockMultipartFile(
 				"myfiletest",
 				"NameOfFileTest",
-				"",
+				"video/mp4",
 				inputStream);
 
-		String fileResponse = fileStorageService.store(file);
+		String fileResponse = fileStorageService.storeFile(file);
 
 		assertThat(fileResponse).isInstanceOf(String.class);
 		assertThat(fileResponse).contains("NameOfFileTest");
 
 	}
+
+    @Test
+    public void shoudLoadReadFileInDirectory() throws IOException {
+        testFileName = "NameOfFileTest";
+        Path filePath = Paths.get(fileStorageProperties.getUploadDir()).resolve(testFileName);
+
+        Resource expectedResource = new UrlResource(filePath.toUri());
+
+        Resource resource = fileStorageService.loadFile(testFileName);
+        assertThat(resource).isEqualTo(expectedResource);
+        File deleteFile = new File(fileStorageProperties.getUploadDir());
+        FileUtils.cleanDirectory(deleteFile);
+
+    }
+
 
 }
